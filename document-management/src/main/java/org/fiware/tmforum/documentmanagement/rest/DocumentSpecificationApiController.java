f067de64e5c1e6d3ec9d4abd77da172d66078ebe
Daniel Ros <d.ros@libelium.com>
2026-07-22 14:10:48 +0200

    (feat) document management added missing fields

--- document-management/src/main/java/org/fiware/tmforum/documentmanagement/rest/DocumentSpecificationApiController.java
+++ document-management/src/main/java/org/fiware/tmforum/documentmanagement/rest/DocumentSpecificationApiController.java
@@ -137,6 +137,8 @@ public class DocumentSpecificationApiController extends AbstractApiController<Do
     protected Mono<DocumentSpecification> getCheckingMono(DocumentSpecification docSpec) {
         List<List<? extends ReferencedEntity>> references = new ArrayList<>();
         references.add(docSpec.getRelatedParty());
+        references.add(docSpec.getConstraint());
+        references.add(docSpec.getEntitySpecRelationship());
 
         return getCheckingMono(docSpec, references)
                 .onErrorMap(throwable -> new TmForumException(
