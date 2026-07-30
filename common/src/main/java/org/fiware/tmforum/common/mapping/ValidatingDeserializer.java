96c7148febf05ad4a440e46424623a883a3d2e8c
Daniel Ros <d.ros@libelium.com>
2026-07-30 17:30:51 +0200

    fix(common): restore missing HashMap import dropped by #137's import-style expansion

--- common/src/main/java/org/fiware/tmforum/common/mapping/ValidatingDeserializer.java
+++ common/src/main/java/org/fiware/tmforum/common/mapping/ValidatingDeserializer.java
@@ -14,6 +14,7 @@ import org.fiware.tmforum.common.exception.SchemaValidationException;
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
